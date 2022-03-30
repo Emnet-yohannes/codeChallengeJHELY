ee7e63861f83bdbea8a242cd3afacb07a598cf3b
Emnet-yohannes <emnet.yohan@gmail.com>
2022-03-30 17:24:20 +0300

    mobile menu added final

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f66600924607cf0"; }
+/******/ 		__webpack_require__.h = function() { return "2a3568ee1a3e3dfc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <SearchBar />
       </div>
 
-      <div className="visible bg-black md:hidden">
+      <div className="visible bg-black mr-[20px] md:hidden">
         <button onClick={()=>{setDrawer(!drawer)}} type="button" class="inline-flex justify-center w-full rounded-md border bg-black border-gray-300 shadow-sm px-2 py-2  text-sm font-medium text-gray-700 hover:bg-black " id="menu-button" aria-expanded="true" aria-haspopup="true">
         <svg class="w-4 h-4" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       <svg class="hidden w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
