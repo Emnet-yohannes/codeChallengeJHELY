e683293124b98d47f1df0e557fba369a98de5cc5
Emnet-yohannes <emnet.yohan@gmail.com>
2022-03-30 16:55:05 +0300

    mobile menu added2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c7c6f13c78f0599"; }
+/******/ 		__webpack_require__.h = function() { return "d032f4347ef1bd68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <SearchBar />
       </div>
 
-      <div className="bg-black">
+      <div className="visible bg-black md:hidden">
         <button onClick={()=>{setDrawer(!drawer)}} type="button" class="inline-flex justify-center w-full rounded-md border bg-black border-gray-300 shadow-sm px-4 py-2  text-sm font-medium text-gray-700 hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
         <svg class="w-6 h-6" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -28,7 +28,7 @@ const Header = () => {
 
 {
   drawer &&
-  <div class="origin-top-right absolute right-[10px] top-[70px] z-10 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class=" origin-top-right absolute right-[10px] top-[70px] z-10 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
         <div class="py-1" role="none">
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">lorem ipsum</a>
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">lorem ipsum</a>
@@ -37,7 +37,7 @@ const Header = () => {
             <button type="submit" class="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
           </form>
         </div>
-      </div>
+    </div>
   
 }
 
@@ -45,7 +45,7 @@ const Header = () => {
 
 
 
-      <div className="hidden flex md:justify-evenly ml-[41px] mr-[40px]">
+      <div className="hidden md:flex md:justify-evenly ml-[41px] mr-[40px]">
         <button className="text-white w-[47px] h-[20px]">Lorem</button>
         <button className="text-white w-[47px] h-[20px] ml-[53px]">
           Lorem
